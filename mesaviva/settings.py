2c1c843d4af2af1bb40d5c63c3dc26d9fc3197ca
Hans Latoja <130710382+NertaiL@users.noreply.github.com>
2026-08-18 12:47:57 -0400

    Update ALLOWED_HOSTS for production and local use

--- mesaviva/settings.py
+++ mesaviva/settings.py
@@ -18,7 +18,15 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# Local with docker
+#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "mesa-viva-yc4s.onrender.com",
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
